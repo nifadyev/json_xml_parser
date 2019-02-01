018f82a440949bdb37572d326d39e50cebf86442
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-01 17:41:19 +0300

    Replace urllib modlue with requests, set deafult output path is it wasn't set

--- xml_to_json.py
+++ xml_to_json.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 from lxml import objectify
 from json import dump
-from urllib.request import urlopen
+import requests
 
 
 def parse(url_to_xml, path_to_output_file):
-    root = objectify.XML(urlopen(url_to_xml).read())
+    root = objectify.XML(requests.get(url_to_xml).content)
     data = {}
 
     for element in root.getchildren():
         data[element.tag] = element.text
 
+    if path_to_output_file == "":
+        path_to_output_file ="output_json.json"
+
     with open(path_to_output_file, "w+") as output:
         dump(data, output)
-
 
 parse("http://ip-api.com/xml/{ip}", "output_json.json")
