7a2d68113b86bef7de2d3e95f539bcd3eeb66308
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-01 17:51:35 +0300

    Add parsing using url to json file
    - Delete extra comments
    - Set default path to output file if it isn't set

--- json_to_xml.py
+++ json_to_xml.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from json import load
-# from dicttoxml import dicttoxml
+import requests
 
 
 def parse(path_to_input_file, path_to_output_file, root="query"):
@@ -15,16 +15,30 @@ def parse(path_to_input_file, path_to_output_file, root="query"):
             result_list.append("<%s>%s</%s>" %
                                (tag_name, loaded_json[tag_name], tag_name))
 
-        # JSON -> XML using external module dicttoxml
-        # xml = dicttoxml(loaded_json, root=True,
-        #                 custom_root="query", attr_type=False)
-        # with open(path_to_output_file, "wb") as output_file:
-        #     output_file.write(xml)
+    result_list.append("</%s>" % (root))
+
+    with open(path_to_output_file, "w+") as output_file:
+        output_file.write("".join(result_list))
+
+def parse_using_url(url_to_input_file, path_to_output_file, root="query"):
+    result_list = list()
+
+    result_list.append('''<?xml version="1.0" encoding="UTF-8"?>''')
+    result_list.append("<%s>" % (root))
+
+    loaded_json = requests.get(url_to_input_file).json()
+    for tag_name in loaded_json:
+        result_list.append("<%s>%s</%s>" %
+                            (tag_name, loaded_json[tag_name], tag_name))
 
     result_list.append("</%s>" % (root))
+
+    if path_to_output_file == "":
+        path_to_output_file = "output_xml.xml"
 
     with open(path_to_output_file, "w+") as output_file:
         output_file.write("".join(result_list))
 
 
 parse("input_json.json", "output_xml.xml")
+parse_using_url("http://ip-api.com/json/%7Bip%7D", "123.xml")
