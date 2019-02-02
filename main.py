17fda4259f25d6486dadffceb438a1deca2d5706
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-02 19:56:01 +0300

    Add documentation to function main

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from requests import get
 
 
 def main():
+    '''Parse command line arguments and execute parsing to desired format
+    if  arguments are correct
+    '''
+
     arg_parser = ArgumentParser()
     arg_parser.add_argument("link", help="link to input file")
     arg_parser.add_argument(
         "format", help="format output file", choices=["json", "xml"])
     arg_parser.add_argument(
-        "-out", "--output", help="path to output file", metavar="", default="")
+        "-out", "--output", help="path to output file", metavar="",
+        default="output")
     args = arg_parser.parse_args()
 
     if get(args.link).status_code != 200:
